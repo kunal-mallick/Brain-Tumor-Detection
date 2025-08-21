66157c2a8469d187ccd3fb707b3d435f9b330832
kunal(asus) <kunalmallicks3@gmail.com>
2025-08-21 13:37:13 +0530

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='The project aims to develop an AI-powered deep learning system for early brain tumor detection and classification using MRI scans. The system will use Convolutional Neural Networks and preprocessing techniques to enhance data quality. The system will use public MRI datasets and evaluate metrics like accuracy, precision, and recall. The project has potential for real-world deployment.',
+    author='Your name (or your organization/company/team)',
+    license='',
+)
